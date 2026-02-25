de9256d517f7a147181f26c94d2962c7c5b3aaa3
Kryakn <aryanshrivastav150@gmail.com>
2026-02-25 18:59:25 +0530

    notes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // Firebase config - hardcoded for production deployment
 // Firebase config - directly embedded for production
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ4k4gT1d4v1hC4k4gT1d4v1hC4k4gT1d4",
+  apiKey: "AIzaSyDjxi_WZ8BXZqo6meuT4miL5M33GORw3jE",
   authDomain: "chatgen-38a7f.firebaseapp.com",
   projectId: "chatgen-38a7f",
-  storageBucket: "chatgen-38a7f.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abc123def456"
+  storageBucket: "chatgen-38a7f.firebasestorage.app",
+  messagingSenderId: "152767825787",
+  appId: "1:152767825787:web:8eab8f9a6a8cc95f5d5fde"
 };
 
 // Check if all required config is present
